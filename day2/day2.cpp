e62bd1b38b4693e6e0d534c67e4fd223547c3809
stas420 <sniemczewski@onet.pl>
2024-12-06 01:25:58 +0100

    day 2 fully done (ugly solve tho) **lacks: day4**

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -10,14 +10,42 @@ const std::string input = "input.txt";
 // currently done by iterating through each report twice;
 // should probably be done with two alternating "pointers";
 // well whatevr
+constexpr int MAX_DIFF = 3;
+
+bool isSafePtOne(const std::vector<std::int64_t>& levelsVec)
+{
+    std::int64_t tmp = 0;
+    bool decreasing = true;
+
+    for (std::int64_t i = 0; i < (levelsVec.size() - 1); i++)
+    {
+        tmp = levelsVec[i] - levelsVec[i + 1];
+
+        if (tmp == 0)
+        {
+            return false;
+        }
+
+        if (i == 0)
+        {
+            decreasing = (tmp > 0);
+        }
+
+        if (((tmp > 0) != decreasing) || abs(tmp) > MAX_DIFF)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 void PartOne()
 {
     std::fstream file(input, std::ios::in);
 
     if (file.is_open())
     {
-        constexpr int MAX_DIFF = 3;
-
         std::string report = "";
         std::uint32_t result = 0;
 
@@ -47,29 +75,7 @@ void PartOne()
                 }
             }
 
-            for (i = 0; i < (levels.size() - 1); i++)
-            {
-                tmp = levels[i] - levels[i + 1];
-
-                if (tmp == 0)
-                {
-                    safe = false;
-                    break;
-                }
-
-                if (i == 0)
-                {
-                    decreasing = (tmp > 0);
-                }
-
-                if (((tmp > 0) != decreasing) || abs(tmp) > MAX_DIFF)
-                {
-                    safe = false; 
-                    break;
-                }
-            }
-
-            if (safe)
+            if (isSafePtOne(levels))
             {
                 result += 1;
             }
@@ -84,14 +90,31 @@ void PartOne()
     }
 }
 
+bool isSafePtTwo(const std::vector<std::int64_t>& levelsVec)
+{
+    if (isSafePtOne(levelsVec))
+        return true;
+
+    std::vector<std::int64_t> tmp;
+
+    for (std::int32_t i = 0; i < levelsVec.size(); i++)
+    {
+        tmp = levelsVec;
+        tmp.erase(tmp.begin() + i);
+
+        if (isSafePtOne(tmp))
+            return true;
+    }
+
+    return false;
+}
+
 void PartTwo()
 {
-        std::fstream file(input, std::ios::in);
+    std::fstream file(input, std::ios::in);
 
     if (file.is_open())
     {
-        constexpr int MAX_DIFF = 3;
-
         std::string report = "";
         std::uint32_t result = 0;
 
@@ -121,35 +144,24 @@ void PartTwo()
                 }
             }
 
-            for (i = 0; i < (levels.size() - 1); i++)
+            if (isSafePtTwo(levels))
             {
-                tmp = levels[i] - levels[i + 1];
-
-                if (tmp == 0)
-                {
-                    safe = false;
-                    break;
-                }
-
-                if (i == 0)
-                {
-                    decreasing = (tmp > 0);
-                }
-
-                if (((tmp > 0) != decreasing) || abs(tmp) > MAX_DIFF)
-                {
-                    safe = false; 
-                    break;
-                }
-            }
-
-            if (safe)
-            {
+                //std::cout << "safe";
                 result += 1;
             }
+            else
+            {
+                levels.erase(levels.begin());
+
+                if (isSafePtOne(levels))
+                    result += 1;
+            }
+
+            //std::cout << std::endl;
         }
 
-        std::cout << "Part one result " << result << std::endl;
+        std::cout << "Part two result " << result << std::endl;
+        // 364
     }
     else
     {
@@ -164,3 +176,71 @@ int main()
 
     return 0;
 }
+
+/*
+
+// first try??? smth brong
+bool isSafePtTwo(const std::vector<std::int64_t>& levelsVec)
+{
+    //std::cout << "--------------\n";
+    std::int64_t tmp = 0;
+    std::vector<std::int64_t> vec(levelsVec);
+    bool isDecreasing = true;
+
+    for (std::int64_t i = 0; i < levelsVec.size(); i++)
+    {
+        tmp = levelsVec[i] - levelsVec[i+1];
+
+        //std::cout << tmp << " = " << levelsVec[i] << " - " << levelsVec[i+1] << "\n";
+
+        if (tmp == 0)
+        {
+            vec.clear();
+            vec = levelsVec;
+            vec.erase(vec.begin() + i);
+            
+            if (!isSafePtOne(vec))
+            {
+                vec = levelsVec;
+                vec.erase(vec.begin() + i + 1);
+
+                if (!isSafePtOne(vec))
+                {
+                    return false;
+                }
+            }
+        }
+
+        if (i == 0)
+        {
+            isDecreasing = (tmp > 0);
+        }
+
+        if (((tmp > 0) != isDecreasing) || abs(tmp) > MAX_DIFF)
+        {
+            vec.clear();
+            vec = levelsVec;
+            vec.erase(vec.begin() + i);
+            
+            if (!isSafePtOne(vec))
+            {
+                vec.clear();
+                vec = levelsVec;
+                vec.erase(vec.begin() + i + 1);
+
+                if (!isSafePtOne(vec))
+                {
+                    return false;
+                }
+
+                break;
+            }
+
+            break;
+        }
+
+    }
+
+    return true;
+}
+*/
