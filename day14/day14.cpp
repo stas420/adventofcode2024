86248a37072825917aec7f3415653be9db0ac730
stas420 <sniemczewski@onet.pl>
2024-12-15 15:29:47 +0100

    done day14 pt1, but what the fok is part 2 ??

--- day14/day14.cpp
+++ day14/day14.cpp
@@ -6,8 +6,8 @@
 #include <map>
 #include <optional>
 
-const std::int64_t spaceWidth = 11; // 11 in example, 101 in exercise
-const std::int64_t spaceHeight = 7; // 7 in example, 103 in exercise
+const std::int64_t spaceWidth = 101; // 11 in example, 101 in exercise
+const std::int64_t spaceHeight = 103; // 7 in example, 103 in exercise
 
 struct Robot 
 {
@@ -123,65 +123,127 @@ void PartOne(const std::uint64_t& Seconds)
         }
     }
 
-    std::int64_t lowerXbound, higherXbound, lowerYbound, higherYbound;
+    std::int64_t lowerXbound = 0;
+    std::int64_t higherXbound = 0;
+    std::int64_t lowerYbound = 0;
+    std::int64_t higherYbound = 0;
 
-    if (spaceWidth % 2 == 0)
+    if (spaceWidth % 2 == 1)
     {
-        lowerXbound = spaceWidth/2;
-        higherXbound = lowerXbound + 1;
-    }
-    else
-    {
-        lowerXbound = spaceWidth/2 + 1;
+        lowerXbound = (spaceWidth - 1)/2;
         higherXbound = lowerXbound;
     }
-
-    if (spaceHeight % 2 == 0)
+    else
     {
-        lowerYbound = spaceHeight/2;
-        higherYbound = lowerYbound + 1;
+        higherXbound = spaceWidth/2;
+        lowerXbound = higherXbound - 1;
+    }
+
+    if (spaceHeight % 2 == 1)
+    {
+        lowerYbound = (spaceHeight - 1)/2;
+        higherYbound = lowerYbound;
     }
     else
     {
-        lowerYbound = spaceHeight/2 + 1;
-        higherYbound = lowerYbound;
+        higherYbound = spaceHeight/2;
+        lowerYbound = higherYbound - 1;
     }
 
-    std::int64_t tmp = 0;
-    std::int64_t ans = 0;
+    std::int64_t qTL = 0;
+    std::int64_t qTR = 0;
+    std::int64_t qDL = 0;
+    std::int64_t qDR = 0;
 
-    iter = TileToRobotCountMap.begin();
-    //while (it)
+    for (auto& tile : TileToRobotCountMap) //< type name too long, may'auto' save me
+    {
+        if (tile.first.first < lowerXbound && tile.first.second < lowerYbound)
+            qTL += tile.second;
+        
+        if (tile.first.first < lowerXbound && tile.first.second > higherYbound)
+            qDL += tile.second;
+        
+        if (tile.first.first > higherXbound && tile.first.second < lowerYbound)
+            qTR += tile.second;
+        
+        if (tile.first.first > higherXbound && tile.first.second > higherYbound)
+            qDR += tile.second;
+    }
 
+    std::cout << "Part one result " << (qTL * qDL * qTR * qDR) << std::endl;
+    // 222901875
+}
+
+void PartTwo()
+{
+    std::int64_t seconds = 17500;
+
+    std::vector<Robot> Robots = ReadAndParseInput();
+    std::map<std::pair<std::int64_t, std::int64_t>, std::int64_t> TileToRobotCountMap;
+    std::pair<std::int64_t, std::int64_t> position;
+    std::map<std::pair<std::int64_t, std::int64_t>, std::int64_t>::iterator iter;
+
+    for (std::int64_t i = 0; i < seconds; i++)
+    {
+        for (Robot& robot : Robots)
+        {
+            position.first = robot.posX + robot.velX;
+            position.second = robot.posY + robot.velY;
+
+            while (position.first >= spaceWidth)
+                position.first -= spaceWidth;
+
+            while (position.first < 0)
+                position.first += spaceWidth;
+
+            while (position.second >= spaceHeight)
+                position.second -= spaceHeight;
+
+            while (position.second < 0)
+                position.second += spaceHeight;
+
+            iter = TileToRobotCountMap.find(position);
+
+            if (iter == TileToRobotCountMap.end())
+            {
+                TileToRobotCountMap.insert({position, 1});
+            }
+            else
+            {
+                iter->second++;
+            }
+        }
+
+        for (int q = 0; q < spaceHeight; q++)
+        {
+            for (int p = 0; p < spaceWidth; p++)
+            {
+                iter = TileToRobotCountMap.find({p, q});
+
+                if (iter == TileToRobotCountMap.end())
+                {
+                    std::cout << ".";
+                } 
+                else
+                {
+                    std::cout << "#";
+                }
+            }
+
+            std::cout << std::endl << std::endl;
+        }
+    }
 }
 
 int main()
 {
-    PartOne(100);
-    //PartTwo();
+    //PartOne(100);
+    PartTwo();
 
     return 0;
 }
 
 /*
-    for (int q = 0; q < spaceHeight; q++)
-    {
-        for (int p = 0; p < spaceWidth; p++)
-        {
-            iter = TileToRobotCountMap.find({p, q});
-
-            if (iter == TileToRobotCountMap.end())
-            {
-                std::cout << ".";
-            } 
-            else
-            {
-                std::cout << iter->second;
-            }
-        }
-
-        std::cout << std::endl;
-    }
 
 
 */
