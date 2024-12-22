250afee587e352681b2eed4f0264575da20718d8
stas420 <sniemczewski@onet.pl>
2024-12-22 18:57:44 +0100

    day17 pt1 done, dunno about pt2 :(

--- day17/day17.cpp
+++ day17/day17.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <cmath>
 
 // this class is poorly written (in terms of beauty and formatting, naturally)
 // well, I was too lazy to make separate header and source file for it lol
@@ -11,33 +12,119 @@ class Computer
     public:
         Computer()
         {
+            this->regA = 0;
+            this->regB = 0;
+            this->regC = 0;
+            this->InstructionPtr = 0;
+            this->Output.clear();
+
             if (!ReadProgram())
             {
                 this->Program.clear();
             }
         }
 
-        std::int64_t GetOperandValue(const std::int64_t& Operand)
+        std::string GetOutput()
         {
-            if (Operand == 4)
-                return this->regA;
-            if (Operand == 5)
-                return this->regB;
-            if (Operand == 6)
-                return this->regC;
-            
-            if (Operand > 6 || Operand < 0)
+            if (this->Output.size() == 0)
+                return "[No output available]";
+
+            std::string _output = "";
+            std::uint64_t i = 0;
+
+            for (i = 0; i < this->Output.size() - 1; i++)
             {
-                std::cout << "Wrong operand value: " << Operand << std::endl;
-                return -1;
+                _output.append(std::to_string(this->Output[i]));
+                _output.append(",");
             }
 
-            return Operand;
+            _output.append(std::to_string(this->Output[i]));
+            return _output;
         }
 
-    //private:
+        void GetComputerState()
+        {
+            std::cout << "-----------------------------------\n"
+                      << "Computer:\n" 
+                      << "Register A: " << this->regA << "\n"           
+                      << "Register B: " << this->regB << "\n"                       
+                      << "Register C: " << this->regC << "\n"
+                      << "Instruction pointer: " << this->InstructionPtr << "\n"
+                      << "Next command(operand): " << static_cast<unsigned int>(this->Program[this->InstructionPtr]) << "("
+                      << static_cast<unsigned int>(this->Program[this->InstructionPtr + 1]) << ")\n"
+                      << "Program: ";
+
+            for (auto& x : this->Program)
+                std::cout << static_cast<unsigned int>(x) << " ";
+
+            std::cout << "\n-----------------------------------\n";
+        }
+
+        void RunProgram(const bool& ifReload)
+        {
+            this->InstructionPtr = 0;
+            this->Output.clear();
+
+            if (ifReload)
+                this->ReadProgram();
+
+            const std::uint64_t programLen = this->Program.size();
+
+            //this->GetComputerState();
+
+            while (InstructionPtr < (programLen - 1) && InstructionPtr >= 0)
+            {
+                if (RunCommand(this->Program[this->InstructionPtr], 
+                               this->Program[this->InstructionPtr + 1]))
+                {
+                    this->InstructionPtr += 2;
+                }
+
+                //this->GetComputerState();
+            }
+        }
+
+        void SetRegisterA(const std::int64_t& newRegisterA)
+        {
+            this->regA = newRegisterA;
+        }
+
+        std::int64_t FindRegisterA()
+        {
+            this->GetComputerState();
+
+            this->RunProgram(true);
+
+            this->GetComputerState();
+
+            if (this->Output == this->Program)
+                return this->regA;
+            
+            while (this->Output.size() != this->Program.size())
+            {
+                this->GetComputerState();
+
+                if (this->Output.size() < this->Program.size())
+                    this->regA += 1;
+                else if (this->Output.size() > this->Program.size())
+                    this->regA -= 1;                
+
+                this->RunProgram(false);
+
+                std::cin.get();
+            }
+
+            //if (this->Output == this->Program)
+                return this->regA;
+
+
+        }
+
+    private:
+        std::uint64_t InstructionPtr;
         std::int64_t regA, regB, regC;
         std::vector<std::uint8_t> Program;
+        std::vector<std::uint8_t> Output;
 
         bool ReadProgram()
         {
@@ -47,6 +134,7 @@ class Computer
             {
                 std::string line;
                 std::int64_t tmp;
+                this->Program.clear();
 
                 std::getline(file, line);
                 tmp = line.find(':');
@@ -68,20 +156,12 @@ class Computer
                 tmp = line.find(':');
                 tmp++;
 
-                for (std::int64_t i = tmp; i < line.length();)
+                for (std::int64_t i = tmp; i < line.length(); i++)
                 {
                     if (isdigit(line[i]))
                     {
-                        std::cout << " >> push back " << atoi(&line[i]);
-                        this->Program.push_back(atoi(&line[i]));
-                        i += std::to_string(Program.back()).length();
+                        this->Program.push_back(std::stoi(line.substr(i, 1)));
                     }
-                    else
-                    {
-                        i++;
-                    }
-
-                    std::cout << std::endl;
                 }
 
                 file.close();
@@ -96,21 +176,113 @@ class Computer
             return false;
         }
 
+        std::int64_t GetComboOperand(const std::int64_t& Operand)
+        {
+            if (Operand == 4)
+                return this->regA;
+            if (Operand == 5)
+                return this->regB;
+            if (Operand == 6)
+                return this->regC;
+            
+            if (Operand > 6 || Operand < 0)
+            {
+                std::cout << "Wrong operand value: " << Operand << std::endl;
+                return -1;
+            }
+
+            return Operand;
+        }
+
+        bool RunCommand(const std::int64_t& Opcode, const std::int64_t& Operand)
+        {
+            bool output = true;
+            
+            switch(Opcode)
+            {
+                case 0: // adv
+                {
+                    this->regA = static_cast<std::int64_t>(this->regA / std::pow(2, this->GetComboOperand(Operand)));
+                    break;
+                }
+                case 1: // bxl
+                {
+                    this->regB = (this->regB ^ Operand);
+                    break;
+                }
+                case 2: // bst
+                {
+                    this->regB = GetComboOperand(Operand) % 8;
+                    break;
+                }
+                case 3: // jnz
+                {
+                    if (this->regA != 0)
+                    {
+                        this->InstructionPtr = Operand;
+                        output = false;
+                    }
+
+                    break;
+                }
+                case 4: // bxc
+                {
+                    this->regB = (this->regB ^ this->regC);
+                    break;
+                }
+                case 5: // out
+                {
+                    this->Output.push_back(GetComboOperand(Operand) % 8);
+                    break;
+                }
+                case 6: // bdv
+                {
+                    this->regB = static_cast<std::int64_t>(this->regA / std::pow(2, this->GetComboOperand(Operand)));
+                    break;
+                }
+                case 7: // cdv
+                {
+                    this->regC = static_cast<std::int64_t>(this->regA / std::pow(2, this->GetComboOperand(Operand)));
+                    break;
+                }
+                default:
+                {
+                    std::cout << "Computer::RunCommand | Unknown Opcode: " << Opcode << std::endl;
+                    break;
+                }
+            }
+
+            return output;
+        }
 };
 
 
 int main()
 {
     Computer bob;
-    
+    //bob.RunProgram(true); //< Part one: 2,3,4,7,5,7,3,0,7
+    //std::cout << "Part one output: " << bob.GetOutput() << std::endl;
+
+    //bob.SetRegisterA(1);
+    //bob.RunProgram(false);
+
+    //bob.SetRegisterA(117440);
+    //bob.RunProgram(false);
+
+    std::cout << "what I found " << bob.FindRegisterA() << std::endl;
+
+    return 0;
+}
+
+/*
+
     std::cout << "Program:\n";
 
-    for (auto& x : bob.Program)
+    for (std::int8_t x : bob.Program)
     {
-        std::cout << x << " ";
+        std::cout << static_cast<unsigned int> (x) << " ";
     }
 
     std::cout << std::endl;
 
-    return 0;
-}
+*/
